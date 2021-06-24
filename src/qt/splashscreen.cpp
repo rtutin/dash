35d3e0eeb4444c287ce90c94e89805f8d3839a93
Ваше Имя <you@example.com>
2021-06-25 02:51:35 +0300

    icons & colors

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -64,7 +64,7 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
 
     // Adjust logo color based on the current theme
     QImage imgLogo = pixmapLogo.toImage().convertToFormat(QImage::Format_ARGB32);
-    QColor logoColor = GUIUtil::getThemedQColor(GUIUtil::ThemedColor::BLUE);
+    QColor logoColor = GUIUtil::getThemedQColor(GUIUtil::ThemedColor::ORANGE);
     for (int x = 0; x < imgLogo.width(); ++x) {
         for (int y = 0; y < imgLogo.height(); ++y) {
             const QRgb rgb = imgLogo.pixel(x, y);
